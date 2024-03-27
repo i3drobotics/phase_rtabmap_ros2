7282126371dd9f9e703daea38b7473c0e8d79757
yip <kinyipchan913@gmail.com>
2024-03-27 08:56:19 +0000

    added rosbad read

--- phase_rtabmap_ros2/phase_read.py
+++ phase_rtabmap_ros2/phase_read.py
@@ -26,40 +26,16 @@ import os
 import numpy as np
 import yaml
 import argparse
+from message_filters import ApproximateTimeSynchronizer, TimeSynchronizer, Subscriber
+import message_filters
 
 # PhaseCameraNode class for camera data capture
 class PhaseCameraNode(Node):
 
     def __init__(self):
-        super().__init__("phase_camera")
+        super().__init__("phase_read")
         self.count_ = 0
-
-        parser = argparse.ArgumentParser()
-        parser.add_argument('--left_serial', type=str, default="40098270", help="Left Serial of Camera")
-        parser.add_argument('--right_serial', type=str, default="40098281", help="Right Serial of Camera")
-        parser.add_argument('--camera_name', type=str, default="746974616e24316", help="Camera Name of Camera")
-        parser.add_argument('--device_type', type=str, default="titania", help="titania or phobos")
-        parser.add_argument('--interface_type', type=str, default="usb", help="usb or gige")
-        parser.add_argument('--exposure', type=int, default=25000, help="Exposure value")
-        args, unknown = parser.parse_known_args()
-        
-        self.left_serial_ = args.left_serial
-        self.right_serial_ = args.right_serial
-        self.camera_name_ = args.camera_name
-
-        if args.device_type == 'titania':
-            self.device_type_ = phase.stereocamera.CameraDeviceType.DEVICE_TYPE_TITANIA
-        elif args.device_type == 'phobos':
-            self.device_type_ = phase.stereocamera.CameraDeviceType.DEVICE_TYPE_PHOBOS
-        else:
-            self.device_type_ = phase.stereocamera.CameraDeviceType.DEVICE_TYPE_GENERIC_PYLON
-            
-        if args.interface_type == 'usb':
-            self.interface_type_ = phase.stereocamera.CameraInterfaceType.INTERFACE_TYPE_USB
-        elif args.interface_type == 'gige':
-            self.interface_type_ = phase.stereocamera.CameraInterfaceType.INTERFACE_TYPE_GIGE
-        else:
-            self.interface_type_ = phase.stereocamera.CameraInterfaceType.INTERFACE_TYPE_VIRTUAL
+        self.node = rclpy.create_node("phase_read")
 
         self.cv_bridge = CvBridge()
 
@@ -67,16 +43,15 @@ class PhaseCameraNode(Node):
         home_path = os.path.expanduser('~')
         package_name = "phase_rtabmap_ros2"
 
-        left_yaml_name = "left"+str(self.camera_name_[-5:])+".yaml"
-        right_yaml_name = "right"+str(self.camera_name_[-5:])+".yaml"
+        left_yaml_name = "left.yaml"
+        right_yaml_name = "right.yaml"
 
         # Define calibration files
         cal_folder = os.path.join(home_path, "ros2_ws", "install", package_name, "share", package_name, "cal")
         left_yaml = os.path.join(cal_folder, left_yaml_name)
         right_yaml = os.path.join(cal_folder, right_yaml_name)
-        
-        # Define parameters for read process
-        self.exposure_value_ = args.exposure
+        # self.imageL = message_filters.Subscriber("/left/camera_info", CameraInfo)
+        # self.imageR = message_filters.Subscriber("/right/camera_info", CameraInfo)
         
         # Check for I3DRSGM license
         license_valid = phase.stereomatcher.StereoI3DRSGM().isLicenseValid()
@@ -100,34 +75,31 @@ class PhaseCameraNode(Node):
         # Create stereo matcher
         self.matcher_ = phase.stereomatcher.createStereoMatcher(stereo_params)
 
-        device_info = phase.stereocamera.CameraDeviceInfo(
-            self.left_serial_, self.right_serial_, self.camera_name_,
-            self.device_type_, self.interface_type_
-        )
-
-        if args.device_type == 'titania':
-            self.cam_ = phase.stereocamera.TitaniaStereoCamera(device_info)
-        elif args.device_type == 'phobos':
-            self.cam_ = phase.stereocamera.PhobosStereoCamera(device_info)
-
         self.left_camerainfo_ = self.yaml_to_camerainfo(left_yaml)
         self.right_camerainfo_ = self.yaml_to_camerainfo(right_yaml)
 
-        ret = self.cam_.connect()
-        if (ret):
-            self.cam_.startCapture()
+        # self.imageL = self.create_subscription(Image, "camera2/left/image_raw", self.read_frame,10)
+        # self.imageR = self.create_subscription(Image, "camera2/right/image_raw", self.read_frame,10)
 
-        self.pub_img_rawleft_ = self.create_publisher(Image, 'left/image_raw', 100)
-        self.pub_img_rawright_ = self.create_publisher(Image, 'right/image_raw', 100)
         self.pub_caminfo_left_ = self.create_publisher(CameraInfo, 'left/camera_info', 100)
         self.pub_caminfo_right_ = self.create_publisher(CameraInfo, 'right/camera_info', 100)
         self.pub_img_left_ = self.create_publisher(Image, 'left/image_rect', 100)
         self.pub_img_right_ = self.create_publisher(Image, 'right/image_rect', 100)
         self.pub_depth_ = self.create_publisher(Image, 'depth/image', 100)
-        self.pub_pointcloud_ = self.create_publisher(PointCloud2, 'points2', 100)
+
+        self.pub_caminfo_left_ = self.create_publisher(CameraInfo, 'left/camera_info', 100)
+        self.pub_caminfo_right_ = self.create_publisher(CameraInfo, 'right/camera_info', 100)
+
+        imageL = Subscriber(self, Image, "left/image_raw")
+        imageR = Subscriber(self, Image, "right/image_raw")
+
+        # ts = ApproximateTimeSynchronizer([imageL,imageR], queue_size=10, slop=0.5, allow_headerless=True)
+        # # # Synchronize images
+        ts = TimeSynchronizer([imageL, imageR], 10)
+        ts.registerCallback(self.read_frame)
         
-        # Publish camera name message every 500ms
-        self.timer_read = self.create_timer(0.1, self.read_frame)
+        # # Publish camera name message every 500ms
+        # self.timer_read = self.create_timer(0.1, self.read_frame)
 
     def yaml_to_camerainfo(self, yaml_file_path):
         # Load data from file
@@ -201,50 +173,48 @@ class PhaseCameraNode(Node):
             data=data
         )
 
-    def read_frame(self):
-        self.cam_.setExposure(self.exposure_value_)
-        read_result = self.cam_.read()
-        if (read_result.valid):
-            self.get_logger().info("Stereo result received")
-            rect_image_pair = self.calibration_.rectify(read_result.left, read_result.right)
-            rect_img_left = rect_image_pair.left
-            rect_img_right = rect_image_pair.right
+    def read_frame(self, imageL, imageR):
+        br = CvBridge()
+        imageLeft = br.imgmsg_to_cv2(imageL)
+        imageRight = br.imgmsg_to_cv2(imageR)
+        self.get_logger().info("Receiving Frames")
+        rect_image_pair = self.calibration_.rectify(imageLeft, imageRight)
+        rect_img_left = rect_image_pair.left
+        rect_img_right = rect_image_pair.right
 
-            match_result = self.matcher_.compute(rect_img_left, rect_img_right)
+        match_result = self.matcher_.compute(rect_img_left, rect_img_right)
 
-            # Check compute is valid
-            if not match_result.valid:
-                print("Failed to compute match")
+        # Check compute is valid
+        if not match_result.valid:
+            print("Failed to compute match")
 
-            # Find the disparity from matcher
-            disparity = match_result.disparity
-            depth = phase.disparity2depth(disparity, self.calibration_.getQ())
+        # Find the disparity from matcher
+        disparity = match_result.disparity
+        depth = phase.disparity2depth(disparity, self.calibration_.getQ())
 
-            header = Header()
-            header.frame_id = "camera_link"
-            header.stamp = self.get_clock().now().to_msg()
+        header = Header()
+        header.frame_id = "camera_link"
+        header.stamp = self.get_clock().now().to_msg()
 
-            # header_pc = Header()
-            # #header_pc.frame_id = "map"
-            # header_pc.stamp = header.stamp
+        # header_pc = Header()
+        # #header_pc.frame_id = "map"
+        # header_pc.stamp = header.stamp
 
-            # self.pc_msg = self.point_cloud(points, 'map')
+        # self.pc_msg = self.point_cloud(points, 'map')
 
-            self.left_camerainfo_.header = header
-            self.right_camerainfo_.header = header
-            #self.pc_msg.header = header_pc
+        self.left_camerainfo_.header = header
+        self.right_camerainfo_.header = header
+        #self.pc_msg.header = header_pc
 
-            self.publish_image(self.pub_img_rawleft_, read_result.left, header = header)
-            self.publish_image(self.pub_img_rawright_, read_result.right, header = header)
-            self.publish_image(self.pub_img_left_, rect_img_left, header = header)
-            self.publish_image(self.pub_img_right_, rect_img_right, header = header)
-            self.publish_image(self.pub_depth_, depth, encoding = "32FC1", header = header)
-            # self.pub_pointcloud_.publish(self.pc_msg)
-            self.pub_caminfo_left_.publish(self.left_camerainfo_)
-            self.pub_caminfo_right_.publish(self.right_camerainfo_)
+        # self.publish_image(self.pub_img_rawleft_, imageLeft, header = header)
+        # self.publish_image(self.pub_img_rawright_, imageRight.right, header = header)
+        self.publish_image(self.pub_img_left_, rect_img_left, header = header)
+        self.publish_image(self.pub_img_right_, rect_img_right, header = header)
+        self.publish_image(self.pub_depth_, depth, encoding = "32FC1", header = header)
+        # self.pub_pointcloud_.publish(self.pc_msg)
+        self.pub_caminfo_left_.publish(self.left_camerainfo_)
+        self.pub_caminfo_right_.publish(self.right_camerainfo_)
 
-        else:
-            self.get_logger().warn("Failed to read stereo result")
 
     def publish_image(self,
             image_pub, image,
